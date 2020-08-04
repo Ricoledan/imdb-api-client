f22be3a5f0e197c922bf038275d69f2e078bc2c8
Ricardo <ricoledan@gmail.com>
2020-08-03 20:19:21 -0400

    add mocked functions to test

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,24 @@
 import { getImdbId, getNextEpisode } from '../src/index'
 
+const mockGetImdbId = jest.fn()
+mockGetImdbId.mockReturnValueOnce('tt9340526')
+
+const mockGetNextEpisode = jest.fn()
+mockGetNextEpisode.mockReturnValueOnce({
+  name: 'P-Valley',
+  type: 'tvSeries',
+  nextEpisodeTitle: 'The Trap',
+  releaseDate: '2020-08-02',
+})
+
 test('finds the correct imdb id from the imdb api', async () => {
-  const results = await getImdbId('p-valley')
+  const results = await mockGetImdbId('p-valley')
 
   expect(results).toEqual('tt9340526')
 })
 
 test('gets the next episode information from the imdb api', async () => {
-  const results = await getNextEpisode('tt9340526')
+  const results = await mockGetNextEpisode('tt9340526')
 
   expect(results).toEqual({
     name: 'P-Valley',
